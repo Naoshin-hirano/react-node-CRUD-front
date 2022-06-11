7afee3ceaec9bfccbad09fc3cc43206d5789857e
Hirano <hnaoya1120@gmail.com>
2022-06-11 15:27:06 +0900

    App.jsをCrud.jsに変更し、rooter設定

--- src/App.js
+++ src/App.js
@@ -1,68 +1,22 @@
-import React, { useState, useEffect } from "react";
-import './App.css';
-import Axios from "axios";
+import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 
-import { Post } from "./components/posts";
+import { Crud } from "./pages/crud";
+import { Authentication } from "./pages/authentication";
 
-function App() {
-  console.log("App.jsのレンダリング");
-  const [movieName, setMovieName] = useState('');
-  const [review, setReview] = useState('');
-  const [movieReviewList, setMovieList] = useState([]);
-
-
-  useEffect(() => {
-      Axios.get("http://localhost:3001/api/get").then((response) => {
-        setMovieList(response.data);
-      })
-  }, []);
-
-  // 投稿ボタン
-  const submitReview = () => {
-      // server側でリクエストのbodyに入ってくる
-      Axios.post("http://localhost:3001/api/insert", {
-          movieName: movieName,
-          movieReview: review
-      });
-      // 配列に新たなオブジェクトを追加
-      setMovieList([...movieReviewList,
-        { movieName: movieName, movieReview: review }
-      ]);
-      setMovieName('');
-      setReview('');
-  };
-
+export const App = () => {
   return (
-    <div className="App">
-        <h1>CRUD APPLICATION</h1>
-
-        <div className="form">
-        <label>Movie Name:</label>
-        <input value={movieName} type="text" name="movieName" onChange={(e) => {
-            setMovieName(e.target.value);
-        }}/>
-        <label>Review</label>
-        <input value={review} type="text" name="review" onChange={(e) => {
-            setReview(e.target.value);
-        }}/>
-
-        <button onClick={submitReview}>Submit</button>
-        {movieReviewList.map((val, index) => {
-           return (
-                <div key={index} className="card">
-                    <Post
-                    index={index}
-                    movieName={val.movieName}
-                    movieReview={val.movieReview}
-                    movieReviewList={movieReviewList}
-                    setMovieList={setMovieList}
-                    />
-                </div>
-            );
-        })}
-        </div>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Crud />
+          </Route>
+          <Route exact path="/auth">
+            <Authentication />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/crud.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import '../App.css';
+import Axios from "axios";
+
+import { Post } from "../components/posts";
+
+export const Crud = () => {
+  console.log("App.jsのレンダリング");
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+  const [movieReviewList, setMovieList] = useState([]);
+
+
+  useEffect(() => {
+      Axios.get("http://localhost:3001/api/get").then((response) => {
+        setMovieList(response.data);
+      })
+  }, []);
+
+  // 投稿ボタン
+  const submitReview = () => {
+      // server側でリクエストのbodyに入ってくる
+      Axios.post("http://localhost:3001/api/insert", {
+          movieName: movieName,
+          movieReview: review
+      });
+      // 配列に新たなオブジェクトを追加
+      setMovieList([...movieReviewList,
+        { movieName: movieName, movieReview: review }
+      ]);
+      setMovieName('');
+      setReview('');
+  };
+
+  return (
+    <div className="App">
+        <h1>CRUD APPLICATION</h1>
+
+        <div className="form">
+        <label>Movie Name:</label>
+        <input value={movieName} type="text" name="movieName" onChange={(e) => {
+            setMovieName(e.target.value);
+        }}/>
+        <label>Review</label>
+        <input value={review} type="text" name="review" onChange={(e) => {
+            setReview(e.target.value);
+        }}/>
+
+        <button onClick={submitReview}>Submit</button>
+        {movieReviewList.map((val, index) => {
+           return (
+                <div key={index} className="card">
+                    <Post
+                    index={index}
+                    movieName={val.movieName}
+                    movieReview={val.movieReview}
+                    movieReviewList={movieReviewList}
+                    setMovieList={setMovieList}
+                    />
+                </div>
+            );
+        })}
+        </div>
+    </div>
+  );
+}
