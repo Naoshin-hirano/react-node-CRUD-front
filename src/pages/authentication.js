0ac1c21e19fc88994cbd55f377f9edb375379088
Hirano <hnaoya1120@gmail.com>
2022-06-12 11:03:10 +0900

    簡単なアカウント登録とログイン（フロント）

--- src/pages/authentication.js
+++ src/pages/authentication.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import '../App.css';
+import Axios from "axios";
 
 export const Authentication = () => {
-    return (
-        <div>ログイン画面</div>
-    );
-};
+  const [usernameReg, setUsernameReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [validation, setValidation] = useState('');
+
+  const register = () => {
+      Axios.post("http://localhost:3001/api/register", {
+          username: usernameReg,
+          password: passwordReg
+      }).then((response) => {
+          console.log(response);
+      });
+  };
+
+  const login = () => {
+    Axios.post("http://localhost:3001/api/login", {
+        username: username,
+        password: password
+    }).then((response) => {
+        if(response.data.message){
+            setValidation(response.data.message);
+        } else {
+            setValidation(response.data[0].username);
+        }
+    });
+  }
+
+  return (
+    <div className="App">
+        <div className="form">
+            <h2>Registration</h2>
+            <label>Username</label>
+            <input type="text" value={usernameReg} onChange={(e) => {setUsernameReg(e.target.value)}}/>
+            <label>Password</label>
+            <input type="text" value={passwordReg} onChange={(e) => {setPasswordReg(e.target.value)}}/>
+            <button onClick={register}>Register</button>
+        </div>
+        <div className="form">
+            <h2>Login</h2>
+            <input type="text" value={username} placeholder="Username..." onChange={(e) => {setUsername(e.target.value)}}/>
+            <input type="text" value={password} placeholder="Password..." onChange={(e) => {setPassword(e.target.value)}}/>
+            <button onClick={login}>Login</button>
+        </div>
+        <h1>{validation}</h1>
+    </div>
+  );
+}
